cda33019d4f9a8841dce14bd87ec6c9191f1b4c5
mohanthilak <mohan.thilak410@gmail.com>
2024-03-10 23:19:26 +0530

    redis cloud url

--- Redis/redis.go
+++ Redis/redis.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"log"
-	"net/url"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -14,20 +13,8 @@ type RedisStruct struct {
 }
 
 func NewRedisClient() *RedisStruct {
-	u, err := url.Parse("redis://red-cnmulbmn7f5s73d94d30:6379")
-	if err != nil {
-		panic(err)
-	}
-	host := u.Hostname()
-	port := u.Port()
-	if port == "" {
-		port = "6379" // Default port if not specified in the URL
-	}
-	client := redis.NewClient(&redis.Options{
-		Addr:     host + ":" + port,
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+	opt, _ := redis.ParseURL("redis://red-cnmulbmn7f5s73d94d30:6379")
+	client := redis.NewClient(opt)
 
 	pong, err := client.Ping(context.Background()).Result()
 	if err != nil {
--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 	user "GoCats/User"
 	"GoCats/server"
 	"flag"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
 	wsChan := make(chan []map[string]interface{})
 	defaultPort := 8000
 	port := flag.Int("port", defaultPort, "port")
+
+	pp := os.Getenv("PORT")
+	var pp2 = *port
+	var err error
+	if pp != "" {
+		pp2, err = strconv.Atoi(pp)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	port = &pp2
 
 	redisController := redispkg.NewRedisClient()
 
